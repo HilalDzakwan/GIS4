2ce0817649c636e63ab27a06f9a9a6196052dad1
Hilal Fakhri Dzakwan <hilaldzakwan08@gmail.com>
2023-10-27 08:56:26 +0700

    tugas 4

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
 import { URLGeoJson } from "./template/template.js";
-import { MakeGeojsonFromAPI, responseData, AddLayerToMAP } from "./controller/controller.js";
+import { MakeGeojsonFromAPI, responseData, AddLayerToMAP, drawer } from "./controller/controller.js";
 import {map} from './config/configpeta.js';
-import {onClosePopupClick,onDeleteMarkerClick,onSubmitMarkerClick,onMapClick,onMapPointerMove,disposePopover} from './controller/popup.js';
+import {onClosePopupClick,onDeleteMarkerClick,onSubmitMarkerClick,onMapClick,onMapPointerMove,disposePopover, GetLonLat} from './controller/popup.js';
 import {onClick} from 'https://jscroot.github.io/element/croot.js';
 import {getAllCoordinates} from './controller/cog.js';
 
@@ -12,14 +12,19 @@ onClick('insertmarkerbutton',onSubmitMarkerClick);
 onClick('hapusbutton',onDeleteMarkerClick);
 onClick('hitungcogbutton',getAllCoordinates);
 
+
 map.on('click', onMapClick);
+// map.on('click', onMapInput)
 map.on('pointermove', onMapPointerMove);
 map.on('movestart', disposePopover);
-    
+
+
 get(URLGeoJson,data => {
     responseData(data)
     let link = MakeGeojsonFromAPI(data)
     // console.log(link)
     // console.log(geojson)
     AddLayerToMAP(link)
+    drawer(link)
 }); 
+
